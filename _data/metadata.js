d40b54289dbb346d480928a58ea77f2e753e40a3
ahmad rifai <riff.blend@gmail.com>
2023-10-14 12:22:53 +0700

    change metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Ahmad Rifai | Mobile and Game developer",
-	url: "https://rifai.github.io/",
+	url: "https://rifai.id/",
 	language: "en",
 	description: "Experienced mobile and game developer specializing in Godot Engine, Unity3D, Flutter and Android development. Browse my portfolio and contact me for app/game development projects.",
 	author: {
 		name: "Ahmad Rifai",
 		email: "arifai.dev@gmail.com",
-		url: "https://rifai.github.io/about-me/"
+		url: "https://rifai.id/about-me/"
 	}
 }
